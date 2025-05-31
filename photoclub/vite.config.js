f234fa318c4115252a37d975e112df40baa03d27
Ernesto <ernestosalonso2023@gmail.com>
2025-05-31 17:47:06 +0200

    modified:   photoclub/vite.config.js

--- photoclub/vite.config.js
+++ photoclub/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: '/photoclub/', // 👈 cambia esto por el nombre de tu repositorio en GitHub
+
 })
